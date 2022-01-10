5a7763cc45da201407a2aee583c571c82e89490e
Louise <louise@zanier.org>
2022-01-11 00:49:59 +0100

    First commit

---
+++ webpack.config.ts
@@ -0,0 +1,74 @@
+import { join, resolve } from 'path';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+const baseDir = resolve(__dirname)
+
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+export interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration
+}
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  filename: './index.html',
+  template: './static/index.html',
+})
+const port = Number(process.env.PORT) || 8080
+
+export const config: Configuration = {
+  devServer: {
+    compress: true,
+    hot: false,
+    liveReload: false,
+    port,
+    static: {
+      directory: join(baseDir, 'dist/src'),
+    },
+  },
+  entry: resolve(baseDir, 'src', 'index.tsx'),
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'ts-loader' },
+        ],
+      },
+      {
+        loader: 'file-loader',
+        options: {
+          esModule: false,
+          name: 'assets/images/[name].[ext]',
+        },
+        test: /\.(png|jpg|gif|ico|svg)$/,
+      },
+      {
+        test: /\.s[sc]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.ttf$/,
+        use: { loader: 'file-loader?name=assets/fonts/[name].[ext]' },
+      },
+    ],
+  },
+  output: {
+    filename: 'index.js',
+    path: resolve(baseDir, 'dist/src'),
+    publicPath: '/',
+  },
+  plugins: [htmlPlugin],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    modules: ['src', 'node_modules'],
+  },
+}
+
+export default config
